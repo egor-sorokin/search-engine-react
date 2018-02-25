e46fc5ba4bb6051d15d99a592133f40827f3553e
Egor Sorokin <>
2018-02-25 09:05:36 +0100

    Create results component update styles for that

---
+++ src/components/results/Results.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+import './Results.css'
+
+
+class Results extends Component {
+  render() {
+    return (
+      <div className="Result">
+        <ul className="Result-list">
+          <li className="Result-item">
+            <a href="/" className="Result-item-link" title="">
+              <div className="Result-item-icon"></div>
+              TBD
+            </a>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Results
