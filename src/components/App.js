5ad646f30a968f9e3efb4a27d8af579f6e703da8
Egor Sorokin <>
2018-02-25 09:06:51 +0100

    Add results and search components to main App

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+
+import Search from './search/Search'
+import Results from './results/Results'
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Search engine</h1>
+          <div className="App-search">
+            <Search></Search>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main className="App-result">
+          <Results></Results>
+        </main>
       </div>
     );
   }
