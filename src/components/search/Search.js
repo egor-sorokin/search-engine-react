42131960822c9d267c3396ae9d9781b7f2b8d93e
Egor Sorokin <>
2018-02-26 10:23:29 +0100

    Move getData to App component

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Async } from 'react-select';
-import { v4 } from 'uuid';
 
 import 'react-select/dist/react-select.css';
 import './Search.css';
@@ -20,72 +19,16 @@ class Search extends Component {
     });
   };
 
-  getData = (input) => {
+  onClick = (e) => {
+    this.props.onSearch(e, this.state.value.title);
+  };
+
+  getOptions = (input) => {
     if (!input) {
       return Promise.resolve({options: []});
     }
 
-    const wikiUrl = `https://en.wikipedia.org//w/api.php?action=opensearch&search=${input}&origin=*`;
-    const NYTimesUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=18cdf939cc484212834e3766d1b63555&q=${input}&format=json`;
-
-    const requestWikiData = fetch(wikiUrl)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        let result = [];
-
-        for (let i = 0, len = data[1].length; i < len; i++) {
-          result.push({
-            'id': v4(),
-            'title': data[1][i],
-            'text': data[2][i],
-            'link': data[3][i]
-          })
-        }
-
-        return result;
-      })
-      .catch((error) => {
-        console.error('Fetch WikiData was failed: ', error);
-      });
-
-    const requestNYTimesData = fetch(NYTimesUrl)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        let result = [];
-
-        for (let i = 0, len = data.response.docs.length; i < len; i++) {
-          result.push({
-            'id': v4(),
-            'title': data.response.docs[i]['headline']['main'],
-            'text': data.response.docs[i]['snippet'],
-            'link': data.response.docs[i]['web_url']
-          })
-        }
-
-        return result;
-      })
-      .catch((error) => {
-        console.error('Fetch NYTimesData was failed: ', error);
-      });
-
-
-    return Promise.all([
-        requestWikiData,
-        requestNYTimesData
-      ])
-      .then((results) => {
-        let wikiData = results[0] || [];
-        let NYTimesData = results[1] || [];
-
-        return {options: [...wikiData, ...NYTimesData]};
-      })
-      .catch(function (err) {
-        console.error('Promise.all error', err);
-      });
+    return this.props.requestData(input);
   };
 
   render() {
@@ -96,12 +39,15 @@ class Search extends Component {
           valueKey="id"
           labelKey="title"
           value={this.state.value}
-          loadOptions={this.getData}
+          loadOptions={this.getOptions}
           onChange={this.onChange}
         />
 
         <div className="Search-button">
-          <a href="/" className="Button" title="Search">
+          <a href="/"
+             className="Button"
+             onClick={this.onClick}
+             title="Search">
             <span className="Button-text">Search</span>
           </a>
         </div>
